cbe077e20d161c999e8f89e89b5be47ca31c7bb5
afterdark1017 <145629633+afterdark1017@users.noreply.github.com>
2024-02-28 00:19:32 -0600

    Update mainRequest.py

--- mainRequest.py
+++ mainRequest.py
@@ -6,10 +6,18 @@ from requestClass import requesting,RequestAlert
 # Paste your different Rpc here
 RPC_1 = 'https://eth1.lava.build/lava-referer-cc9e5c72-63b2-4cb2-a184-2da02275e79f/' #  vxlt
 RPC_2 = 'https://eth1.lava.build/lava-referer-bb51f107-9227-4755-afbc-5eac2d67b731/' # X37
-RPC_3 = 'https://eth1.lava.build/lava-referer-80954d5b-d806-40d6-a5f3-d7f6c52995ae/'
-RPC_4 = 'https://eth1.lava.build/lava-referer-e5e7adad-d677-4580-806d-bd2d2e3dd67e/'
-RPC_5 = 'https://eth1.lava.build/lava-referer-85290b4d-eb65-40d4-816b-a998caa660f4/'
+
+RPC_3 = 'https://eth1.lava.build/lava-referer-cc9e5c72-63b2-4cb2-a184-2da02275e79f/'
+# RPC_3 = 'https://eth1.lava.build/lava-referer-80954d5b-d806-40d6-a5f3-d7f6c52995ae/'
+
+RPC_4 = 'https://eth1.lava.build/lava-referer-cc9e5c72-63b2-4cb2-a184-2da02275e79f/'
+# RPC_4 = 'https://eth1.lava.build/lava-referer-e5e7adad-d677-4580-806d-bd2d2e3dd67e/'
+
+RPC_5 = 'https://eth1.lava.build/lava-referer-cc9e5c72-63b2-4cb2-a184-2da02275e79f/'
+# RPC_5 = 'https://eth1.lava.build/lava-referer-85290b4d-eb65-40d4-816b-a998caa660f4/'
+
 RPC_6 = 'https://eth1.lava.build/lava-referer-bcaf1744-0d7b-46b8-a081-fa27b794238a/'
+
 
 requestTasks = [ 'task1','task2' 'task3','task4','task5','task6']
 ENDPOINT = [ RPC_1,RPC_2,RPC_3,RPC_4,RPC_5,RPC_6 ]
